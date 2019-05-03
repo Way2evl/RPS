c5f2b0e1c023a1cad934e24c5fe03f5c0c0a9118
Brian <beliteservie@gmail.com>
2019-05-03 13:41:13 -0700

    made changes capitals, made abb links, removed sys close

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,33 +1,22 @@
-#Fun new project of rock, paper, scissors
-
-
 from random import randint
 
-capitalized_letters_removed_inactive_syntax
 RPS_LIST = ["ROCK", "PAPER", "SCISSORS"]
-CHOICES_DICT = {"R": "ROCK", "P": "PAPER", "S": "SCISSORS", "Q": "QUIT"}
+CHOICES_DICT = {"R": "ROCK", "P": "PAPER", "S": "SCISSORS"}
 user_name = input("Please enter your name:")
 
-=======
-RPS_LIST = ["rock", "paper", "scissors"]
-CHOICES_DICT = {"r": "rock", "p": "paper", "s": "scissors", "q": "quit"}
-user_name = input("Please enter your name:")
-
-while True:
-    raw_user_choice = input("\n\n Hey " + user_name + ", please choose r, p, or s:")
-master
+QT_MSG = 'It was nice beating you'
+WTF_MSG = 'WTF Dude...'
 
 while True:
     
     user_choice = input("\nHey " + user_name + ", please choose R, P, or S:").upper()
     
     if user_choice not in CHOICES_DICT:
-        print('WTF Dude...')
+        print(WTF_MSG)
         continue
-capitalized_letters_removed_inactive_syntax
 
     if user_choice == 'Q':
-        print('It was nice beating you')
+        print(QT_MSG)
         exit()
 
   
@@ -35,18 +24,10 @@ capitalized_letters_removed_inactive_syntax
     cpu_choice = RPS_LIST[randint(0, 2)]
 
     print('You chose "' + user_choice + '" and the computer chose "' + cpu_choice + '"\n')
-=======
-        
-    user_choice = CHOICES_DICT[raw_user_choice]  # r -> rock
-    cpu_choice = RPS_LIST[randint(0, 2)]    # 0 -> rock
-
-    print('You chose "' + user_choice + '" and the computer chose "' + cpu_choice + '"')
-master
 
     if user_choice == cpu_choice:
         print('{} Ties!'.format(user_name))
 
-capitalized_letters_removed_inactive_syntax
     elif (user_choice == 'ROCK' and cpu_choice == 'SCISSORS') or \
     	(user_choice == 'PAPER' and cpu_choice == 'ROCK') or \
     	(user_choice == 'SCISSORS' and cpu_choice == 'PAPER'):
@@ -65,14 +46,3 @@ capitalized_letters_removed_inactive_syntax
         
         
        
-=======
-    elif (user_choice == 'rock' and cpu_choice == 'scissors') or \
-    	(user_choice == 'paper' and cpu_choice == 'rock') or \
-    	(user_choice == 'scissors' and cpu_choice == 'paper'):
-        print('{} Wins!'.format(user_name))
-      
-    elif (user_choice == 'rock' and cpu_choice == 'paper') or \
-    	(user_choice == 'paper' and cpu_choice == 'scissors') or \
-    	(user_choice == 'scissors' and cpu_choice == 'rock'):
-      	print('{} Loses!'.format(user_name))
-master
