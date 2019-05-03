a76603333ec2d1b02afe982ca33a00435e135727
Brian <beliteservie@gmail.com>
2019-05-03 15:11:51 -0700

    Simplified and cleaned up code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,36 +1,47 @@
 from random import randint
-from sys import exit
 
-RPS_LIST = ["rock", "paper", "scissors"]
-CHOICES_DICT = {"r": "rock", "p": "paper", "s": "scissors", "q": "quit"}
+ROCK = 'Rock'
+PAPER = 'PAPER'
+SCISSORS = 'SCISSORS'
+RPS_LIST = [ROCK, PAPER, SCISSORS]
+CHOICES_DICT = {"R": ROCK, "P": PAPER, "S": SCISSORS}
+
+# UI elements
 user_name = input("Please enter your name:")
+QT_MSG = 'It was nice beating you'
+WTF_MSG = 'WTF Dude...'
+CHOOSE_MSG = "\nHey {}, please choose R, P, or S:"
+CHOSE_MSG = 'You chose "{}" and the computer chose "{}"\n'
+WIN_MSG = '{} Wins!'
+LOSE_MSG = '{} Loses!'
+TIE_MSG = '{} Ties!'
+
 
 while True:
-    raw_user_choice = input("\n\n Hey " + user_name + ", please choose r, p, or s:")
 
-    # let them quit
-    if raw_user_choice == 'q':
-        print('It was nice beating you')
+    user_choice = input(CHOOSE_MSG.format(user_name)).upper()
+
+    if user_choice == 'Q':
+        print(QT_MSG)
         exit()
-    
-    if raw_user_choice not in CHOICES_DICT:
-        print("WTF dude...\n\n")
+
+    if user_choice not in CHOICES_DICT:
+        print(WTF_MSG)
         continue
-        
-    user_choice = CHOICES_DICT[raw_user_choice]  # r -> rock
-    cpu_choice = RPS_LIST[randint(0, 2)]    # 0 -> rock
 
-    print('You chose "' + user_choice + '" and the computer chose "' + cpu_choice + '"')
+    user_choice = CHOICES_DICT[user_choice]
+    cpu_choice = RPS_LIST[randint(0, 2)]  
 
+    print(CHOSE_MSG.format(user_choice, cpu_choice))
     if user_choice == cpu_choice:
-        print('{} Ties!'.format(user_name))
+        print(TIE_MSG.format(user_name))
 
-    elif (user_choice == 'rock' and cpu_choice == 'scissors') or \
-    	(user_choice == 'paper' and cpu_choice == 'rock') or \
-    	(user_choice == 'scissors' and cpu_choice == 'paper'):
-        print('{} Wins!'.format(user_name))
-      
-    elif (user_choice == 'rock' and cpu_choice == 'paper') or \
-    	(user_choice == 'paper' and cpu_choice == 'scissors') or \
-    	(user_choice == 'scissors' and cpu_choice == 'rock'):
-      	print('{} Loses!'.format(user_name))
+    elif ((user_choice == ROCK and cpu_choice == SCISSORS) or
+          (user_choice == PAPER and cpu_choice == ROCK) or
+          (user_choice == SCISSORS and cpu_choice == PAPER)):
+        print(WIN_MSG.format(user_name))
+
+    elif ((user_choice == ROCK and cpu_choice == PAPER) or
+          (user_choice == PAPER and cpu_choice == SCISSORS) or
+          (usr_choice == SCISSORS and cpu_choice == ROCK)):
+      	print(LOSE_MSG.format(user_name))
