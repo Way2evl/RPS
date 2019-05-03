efdd5f07612985b79e17ca8ed9c7425e64be24fa
Brian <beliteservie@gmail.com>
2019-05-03 11:06:43 -0700

    capitalized all letters and removed inactive syntax

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,36 +1,49 @@
-from random import randint
-from sys import exit
+#Fun new project of rock, paper, scissors
 
-RPS_LIST = ["rock", "paper", "scissors"]
-CHOICES_DICT = {"r": "rock", "p": "paper", "s": "scissors", "q": "quit"}
+
+from random import randint
+
+RPS_LIST = ["ROCK", "PAPER", "SCISSORS"]
+CHOICES_DICT = {"R": "ROCK", "P": "PAPER", "S": "SCISSORS", "Q": "QUIT"}
 user_name = input("Please enter your name:")
 
-while True:
-    raw_user_choice = input("\n\n Hey " + user_name + ", please choose r, p, or s:")
 
-    # let them quit
-    if raw_user_choice == 'q':
+while True:
+    
+    user_choice = input("\nHey " + user_name + ", please choose R, P, or S:").upper()
+    
+    if user_choice not in CHOICES_DICT:
+        print('WTF Dude...')
+        continue
+
+    if user_choice == 'Q':
         print('It was nice beating you')
         exit()
-    
-    if raw_user_choice not in CHOICES_DICT:
-        print("WTF dude...\n\n")
-        continue
-        
-    user_choice = CHOICES_DICT[raw_user_choice]  # r -> rock
-    cpu_choice = RPS_LIST[randint(0, 2)]    # 0 -> rock
 
-    print('You chose "' + user_choice + '" and the computer chose "' + cpu_choice + '"')
+  
+    user_choice = CHOICES_DICT[user_choice]
+    cpu_choice = RPS_LIST[randint(0, 2)]
+
+    print('You chose "' + user_choice + '" and the computer chose "' + cpu_choice + '"\n')
 
     if user_choice == cpu_choice:
         print('{} Ties!'.format(user_name))
 
-    elif (user_choice == 'rock' and cpu_choice == 'scissors') or \
-    	(user_choice == 'paper' and cpu_choice == 'rock') or \
-    	(user_choice == 'scissors' and cpu_choice == 'paper'):
+    elif (user_choice == 'ROCK' and cpu_choice == 'SCISSORS') or \
+    	(user_choice == 'PAPER' and cpu_choice == 'ROCK') or \
+    	(user_choice == 'SCISSORS' and cpu_choice == 'PAPER'):
         print('{} Wins!'.format(user_name))
+        
       
-    elif (user_choice == 'rock' and cpu_choice == 'paper') or \
-    	(user_choice == 'paper' and cpu_choice == 'scissors') or \
-    	(user_choice == 'scissors' and cpu_choice == 'rock'):
+    elif (user_choice == 'ROCK' and cpu_choice == 'PAPER') or \
+    	(user_choice == 'PAPER' and cpu_choice == 'SCISSORS') or \
+    	(user_choice == 'SCISSORS' and cpu_choice == 'ROCK'):
       	print('{} Loses!'.format(user_name))
+      	
+        
+      
+        
+      
+        
+        
+       
